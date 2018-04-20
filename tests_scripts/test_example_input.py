a5408d9c7dc3cf212c17fe8f3c622a82a307295b
Arigowin <dolewski@student.42.fr>
2018-04-20 14:40:25 +0200

    Improve test

--- tests_scripts/test_example_input.py
+++ tests_scripts/test_example_input.py
@@ -1,41 +1,101 @@
-import os
-print("\n", __file__)
+import sys
 from subprocess import Popen, PIPE
 
+print("\n", __file__)
+arg = sys.argv
+i = 1
 
-for i in range(100):
+if (len(arg) > 1):
+    i = int(arg[1])
+
+for i in range(i):
     process = Popen(["python3", "main.py", "tests/example_input.txt"], stdout=PIPE)
+
     (ret, err) = process.communicate()
+
     exit_code = process.wait()
-    if (str(ret).find("[A:[1, ") == -1 or
-        str(ret).find("[B:[1, ") == -1 or
-        str(ret).find("[C:[1, ") == -1 or
-        str(ret).find("[D:[1, ") == -1 or
-        str(ret).find("[E:[1, ") == -1 or
-        str(ret).find("[F:[0, ") == -1 or
-        str(ret).find("[G:[1, ") == -1 or
-        str(ret).find("[H:[1, ") == -1 or
-        str(ret).find("[I:[0, ") == -1 or
-        str(ret).find("[J:[0, ") == -1 or
-        str(ret).find("[K:[0, ") == -1 or
-        str(ret).find("[L:[0, ") == -1 or
-        str(ret).find("[M:[0, ") == -1 or
-        str(ret).find("[N:[0, ") == -1 or
-        str(ret).find("[O:[0, ") == -1 or
-        str(ret).find("[P:[0, ") == -1 or
-        str(ret).find("[Q:[0, ") == -1 or
-        str(ret).find("[R:[0, ") == -1 or
-        str(ret).find("[S:[0, ") == -1 or
-        str(ret).find("[T:[0, ") == -1 or
-        str(ret).find("[U:[0, ") == -1 or
-        str(ret).find("[V:[2, ") == -1 or
-        str(ret).find("[W:[0, ") == -1 or
-        str(ret).find("[X:[2, ") == -1 or
-        str(ret).find("[Y:[1, ") == -1 or
-        str(ret).find("[Z:[1, ") == -1):
-        print("ET NOP!")
+
+    if str(ret).find("[A:[1, ") == -1:
+        print("Diff on A")
+        b = True
+    if str(ret).find("[B:[1, ") == -1:
+        print("Diff on B")
+        b = True
+    if str(ret).find("[C:[1, ") == -1:
+        print("Diff on C")
+        b = True
+    if str(ret).find("[D:[1, ") == -1:
+        print("Diff on D")
+        b = True
+    if str(ret).find("[E:[1, ") == -1:
+        print("Diff on E")
+        b = True
+    if str(ret).find("[F:[0, ") == -1:
+        print("Diff on F")
+        b = True
+    if str(ret).find("[G:[1, ") == -1:
+        print("Diff on G")
+        b = True
+    if str(ret).find("[H:[1, ") == -1:
+        print("Diff on H")
+        b = True
+    if str(ret).find("[I:[0, ") == -1:
+        print("Diff on I")
+        b = True
+    if str(ret).find("[J:[0, ") == -1:
+        print("Diff on J")
+        b = True
+    if str(ret).find("[K:[0, ") == -1:
+        print("Diff on K")
+        b = True
+    if str(ret).find("[L:[0, ") == -1:
+        print("Diff on L")
+        b = True
+    if str(ret).find("[M:[0, ") == -1:
+        print("Diff on M")
+        b = True
+    if str(ret).find("[N:[0, ") == -1:
+        print("Diff on N")
+        b = True
+    if str(ret).find("[O:[0, ") == -1:
+        print("Diff on O")
+        b = True
+    if str(ret).find("[P:[0, ") == -1:
+        print("Diff on P")
+        b = True
+    if str(ret).find("[Q:[0, ") == -1:
+        print("Diff on Q")
+        b = True
+    if str(ret).find("[R:[0, ") == -1:
+        print("Diff on R")
+        b = True
+    if str(ret).find("[S:[0, ") == -1:
+        print("Diff on S")
+        b = True
+    if str(ret).find("[T:[0, ") == -1:
+        print("Diff on T")
+        b = True
+    if str(ret).find("[U:[0, ") == -1:
+        print("Diff on U")
+        b = True
+    if str(ret).find("[V:[2, ") == -1:
+        print("Diff on V")
+        b = True
+    if str(ret).find("[W:[0, ") == -1:
+        print("Diff on W")
+        b = True
+    if str(ret).find("[X:[2, ") == -1:
+        print("Diff on X")
+        b = True
+    if str(ret).find("[Y:[1, ") == -1:
+        print("Diff on Y")
+        b = True
+    if str(ret).find("[Z:[1, ") == -1:
+        print("Diff on Z")
+        b = True
+
+    if b is True:
         print(ret)
         break
-    print("%d " % i, end='')
 
-
+    print("%d " % (i + 1), end='', flush=True)
